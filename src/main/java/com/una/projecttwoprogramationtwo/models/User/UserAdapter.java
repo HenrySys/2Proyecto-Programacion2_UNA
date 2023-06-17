57a7e318c7554f0f3ef78b255b8083fac9ce283b
Israel <Israel@DESKTOP-K0TFN1O>
2023-06-16 22:45:25 -0500

    hola

--- src/main/java/com/una/projecttwoprogramationtwo/models/User/UserAdapter.java
+++ src/main/java/com/una/projecttwoprogramationtwo/models/User/UserAdapter.java
@@ -1,4 +1,3 @@
-
 package com.una.projecttwoprogramationtwo.models.User;
 
 import java.io.EOFException;
@@ -9,50 +8,47 @@ import java.util.ArrayList;
 import java.io.ObjectInputStream;
 import java.io.FileInputStream;
 
-
 public class UserAdapter {
-    public UserAdapter(){
-        
+
+    public UserAdapter() {
+
     }
-    
-    protected void write(ArrayList<User> list)throws Exception{
+
+    protected void write(ArrayList<User> list) throws Exception {
+        
         ObjectOutputStream out = null;
         try {
             out = new ObjectOutputStream(
                     new FileOutputStream("Users.frishech"));
-            for(int i=0;i<list.size();i++){
+            for (int i = 0; i < list.size(); i++) {
                 out.writeObject(list.get(i));
             }
-            
         } catch (IOException ex) {
             throw new Exception("Error de Escritura de Archivo");
-        }finally{
+        } finally {
             out.close();
         }
-        
     }
-    
-    protected ArrayList<User> read() throws Exception{
+
+    protected ArrayList<User> read() throws Exception {
         ObjectInputStream in;
         ArrayList<User> list = new ArrayList<>();
         try {
             in = new ObjectInputStream(new FileInputStream("Users.frishech"));
-            if(in!=null){
-                User objUser= (User)in.readObject();
-                while (objUser!=null) {                    
+            if (in != null) {
+                User objUser = (User) in.readObject();
+                while (objUser != null) {
                     list.add(objUser);
-                    objUser =(User)in.readObject();
+                    objUser = (User) in.readObject();
                 }
-                
             }
             in.close();
-        } catch (IOException|ClassNotFoundException ex) {
+        } catch (IOException | ClassNotFoundException ex) {
             if (!(ex instanceof EOFException)) {
                 throw new Exception(ex.getMessage());
             }
-        } 
+        }
         return list;
     }
-    
-    
+
 }
